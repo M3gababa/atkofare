00f5d855c65fb9fe33ea73f2e950c1994472631e
Baptiste Duvey <M3gababa@users.noreply.github.com>
2022-10-05 11:28:07 -0500

    Adding logs for demonstration

--- badge.js
+++ badge.js
@@ -1,15 +1,16 @@
 async function nfcRead() {   
     try {
-      	console.log("initiating NFC reader");
+      	console.log("Initiating NFC reader");
         const ndef = new NDEFReader();
         await ndef.scan();
-      	console.log("listening");
+      	console.log("Listening...");
         
         ndef.addEventListener("readingerror", () => {
             console.log("NFC tag not supported. Try another one.");
         });
 
         ndef.addEventListener("reading", ({ message, serialNumber }) => {
+	    console.log("Serial number is : " + serialNumber);
             var badgeId = parseInt(serialNumber.replaceAll(":",""), 16);
             
             var inputs = document.getElementsByTagName('input');
@@ -26,6 +27,7 @@ async function nfcRead() {
                     case 'checkbox':
                         break;
                     case 'submit':
+			console.log("Submit connexion...");
                         field.click();
                         break;
                     default:
@@ -35,15 +37,15 @@ async function nfcRead() {
         });
     } catch (error) {
         console.log("Issue : " + error);
-        document.getElementById("authcontainer").addEventListener("click",async () => {
-			try {
-    			const ndefb = new NDEFReader();
-    			await ndefb.scan();
-    			location.reload();
-			} catch (error) {
-				 //log("Argh! " + error);
-			}
-		});
+        document.getElementById("authcontainer").addEventListener("click", async () => {
+		try {
+			const ndefb = new NDEFReader();
+			await ndefb.scan();
+			location.reload();
+		} catch (error) {
+			 //log("Argh! " + error);
+		}
+	});
     }
 }
 
